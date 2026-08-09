8730b3bc33903ddd1f505913e56bd64bb7b3bd1d
Nimesh Nilashan <nimesh.nilashan@gmail.com>
2026-08-09 23:19:47 +0530

    fix: allow frontend CORS requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,11 +6,24 @@ from sqlalchemy.orm import Session
 from app.config.database import get_db
 
 from app.routers import scans,finding, fim, port_scanner, osint, log_analyzer, dashboard, clerk_webhook
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Argus Security Platform",
     version="1.0.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://argus-security-platform.vercel.app",
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(scans.router)
 app.include_router(finding.router)
